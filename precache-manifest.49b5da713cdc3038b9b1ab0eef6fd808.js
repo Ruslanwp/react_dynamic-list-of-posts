fc58bd740c29a7a4fd7745e101b7a2a48a7f946d
Ruslan Shmadchenko <ruslanwpua@gmail.com>
2021-04-13 00:02:05 +0300

    Updates

---
+++ precache-manifest.49b5da713cdc3038b9b1ab0eef6fd808.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b33a38b637c92f591ddf8173c7b0042",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7d8b0c4e8cae7a25b8b0",
+    "url": "./static/css/main.f8a7786f.chunk.css"
+  },
+  {
+    "revision": "2adca22bff259e3bb59a",
+    "url": "./static/js/2.0aa47ac2.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.0aa47ac2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d8b0c4e8cae7a25b8b0",
+    "url": "./static/js/main.83291bb4.chunk.js"
+  },
+  {
+    "revision": "93b65d15acd366da5855",
+    "url": "./static/js/runtime-main.2badbef3.js"
+  }
+]);
